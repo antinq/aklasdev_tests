b293cf095de20beb67d00c30113ba16d2a56970c
antinq <tylor@ukr.net>
2021-04-13 12:18:28 +0300

    Add domain.Calc.java and appropriate test

---
+++ src/main/java/domain/Calc.java
@@ -0,0 +1,9 @@
+package domain;
+
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+public class Calc {
+    public String sum(String expression){
+        throw new NotImplementedException();
+    }
+}
